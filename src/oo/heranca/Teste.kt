9d5185e0c1eb93111808cd02726677cac7517419
Bruno Cesar <bcesar.bruno@gmail.com>
2019-11-28 18:23:59 -0300

    Finalizado modulo de Orientacao ao Objeto

---
+++ src/oo/heranca/Teste.kt
@@ -0,0 +1,16 @@
+package oo.heranca
+
+fun main() {
+    val carro = Ferrari()
+    carro.acelerar()
+    println(carro)
+
+    carro.ligarTurbo()
+    carro.acelerar()
+    println(carro)
+
+    carro.frear()
+    carro.frear()
+    carro.frear()
+    println(carro)
+}
